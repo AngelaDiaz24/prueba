54089f09fdb09145a66d289240232b31d8b8f25c
rosar <rosar@Rosario>
2025-05-26 13:32:11 -0500

    Agregue mi clase

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -12,6 +12,7 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Te amo ");
+        System.out.println("hi");
         
         
         
